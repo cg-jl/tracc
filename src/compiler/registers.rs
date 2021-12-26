fe94d7acd0db145c80f1a990bd28a667214bbd00
Cyber Gsus <cyber.gsuscode@gmail.com>
2021-12-26 15:19:04 +0100

    remove TODO from src/compiler/registers.rs

--- src/compiler/registers.rs
+++ src/compiler/registers.rs
@@ -2,10 +2,6 @@ use super::{stack::StackManager, AssemblyOutput, CompileWith};
 use crate::assembly::*;
 use std::ops::Try;
 
-// NOTE: when this happens; have the register manager not let mutable access with read-only access.
-//
-// TODO(#10): use static arrays for register maps since the amount of registers available is already known
-
 pub fn with_registers<F, O>(stack: &mut StackManager, cont: F) -> O
 where
     F: FnOnce(&mut StackManager, &mut RegisterManager) -> O,
