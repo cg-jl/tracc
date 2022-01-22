7053dc5950c6d51c11704af593b17853880be183
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-01-22 12:46:33 +0100

    codegen/memory: return indices rather than references on `find_*`

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,23 +1,29 @@
-use crate::intermediate::{BasicBlock, Binding, IRCode, Statement, Value};
+use crate::intermediate::BasicBlock;
+use crate::intermediate::{Binding, IRCode, Statement};
 
 pub mod assembly;
 mod memory;
 
 /// find the definition for a particular binding in a block
-fn find_definition_in_block(block: &BasicBlock, binding: Binding) -> Option<&Value> {
-    for stmt in &block.statements {
-        if let Statement::Assign { index, value } = stmt {
-            if index == &binding {
-                return Some(value);
+fn find_definition_in_block(block: &BasicBlock, binding: Binding) -> Option<usize> {
+    block
+        .statements
+        .iter()
+        .enumerate()
+        .find_map(|(statement_index, statement)| {
+            if let Statement::Assign { index, value } = statement {
+                if index == &binding {
+                    return Some(statement_index);
+                }
             }
-        }
-    }
-    None
+            None
+        })
 }
 
 /// find the definition for a particular binding in the code
-fn find_definition_in_code(code: &IRCode, binding: Binding) -> Option<&Value> {
-    code.iter()
-        .map(|block| find_definition_in_block(block, binding))
-        .fold(None, Option::or)
+fn find_definition_in_code(code: &IRCode, binding: Binding) -> Option<(usize, usize)> {
+    code.iter().enumerate().find_map(|(block_index, block)| {
+        find_definition_in_block(block, binding)
+            .map(|statement_index| (block_index, statement_index))
+    })
 }
