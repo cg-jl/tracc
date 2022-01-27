165d2783e288007194008c48c42114cdcb0253b8
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-01-27 23:22:49 +0100

    intermediate: separate analysis fns into their own module

---
+++ src/intermediate/analysis/mod.rs
@@ -0,0 +1,13 @@
+use crate::intermediate::BasicBlock;
+
+use super::{BlockBinding, BlockEnd, BranchingMap};
+// leaf blocks are blocks that have predecessors but aren't parents of anything
+pub fn find_leaf_blocks<'code>(
+    forward_map: &'code BranchingMap,
+    backwards_map: &'code BranchingMap,
+) -> impl Iterator<Item = BlockBinding> + 'code {
+    backwards_map
+        .keys()
+        .copied()
+        .filter(move |key| !forward_map.contains_key(key))
+}
