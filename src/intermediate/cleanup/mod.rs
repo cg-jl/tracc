dde7be44befda4d59688ae5c6deb2108fa8ef750
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-01-27 23:21:24 +0100

    intermediate/cleanup: rewrite the block pruning algorithm

--- src/intermediate/cleanup/mod.rs
+++ src/intermediate/cleanup/mod.rs
@@ -1,17 +1,13 @@
 use std::collections::HashMap;
 
-use crate::intermediate::{cleanup::redefine::Rename, Statement, Value};
+use super::{
+    refactor::{self, redefine::Rename},
+    BlockBinding, IRCode, Statement, Value, IR,
+};
 
-use super::BackwardsMap;
-use super::BlockBinding;
-use super::IRCode;
-use super::IR;
-
-mod redefine;
-
-pub fn perform_cleanup(ir: &mut IR) {
+pub fn prepare_for_codegen(ir: &mut IR) {
     remove_aliases(&mut ir.code);
-    prune_unreached_blocks(&ir.backwards_map, &mut ir.code);
+    prune_unreached_blocks(ir);
 }
 
 /// final cleanup before codegen: Remove all aliasing behavior
@@ -46,24 +42,105 @@ fn remove_aliases(code: &mut IRCode) {
 }
 
 /// prune not reached blocks
-fn prune_unreached_blocks(backwards_map: &BackwardsMap, code: &mut IRCode) {
-    // list of unused block indices can have up to length of code -1 since the first block (root
-    // block) is always used
-    let mut unused_blocks = Vec::with_capacity(code.len() - 1);
-    for i in 1..code.len() {
-        // no other block reaches this one
-        if backwards_map.get(&BlockBinding(i)).is_none() {
-            unused_blocks.push(i);
-        }
-    }
+fn prune_unreached_blocks(ir: &mut IR) {
+    // #1. Walk the CFG and prune unreached blocks from the map
+    let unused_blocks = {
+        let mut unreached = Vec::new();
 
-    unused_blocks.sort_unstable(); // don't care of order of equal elements... there are none equal elements.
-    for offsetted_index in unused_blocks
-        .into_iter()
-        .rev()
-        .enumerate()
-        .map(|(offset, index)| index - offset)
-    {
-        code.remove(offsetted_index);
+        // start by getting the 'not even registered' blocks.
+        let mut queue: Vec<_> = (1..ir.code.len())
+            .map(BlockBinding)
+            .filter(|binding| !ir.backwards_map.contains_key(binding))
+            .collect();
+
+        // now propagate to their children
+        while !queue.is_empty() {
+            let next = queue.pop().unwrap();
+            if unreached.contains(&next) {
+                continue;
+            }
+            unreached.push(next);
+
+            // for the branches it may have, remove this block from its parents list
+            // and if it ends up empty then add it to the queue
+            for branch_target in ir.forward_map.remove(&next).into_iter().flatten() {
+                // #1. Remove its parent
+                let parents = ir
+                    .backwards_map
+                    .get_mut(&branch_target)
+                    .expect("Malformed backwards graph: no parents assign for a known child");
+
+                parents.retain(|x| x != &next);
+
+                // #2. If the parents are empty, remove the backwards map entry and add it to the
+                // queue
+                if parents.is_empty() {
+                    ir.backwards_map.remove(&branch_target);
+                    queue.push(branch_target);
+                }
+            }
+        }
+
+        unreached.sort_unstable();
+        unreached.reverse();
+        unreached
+    };
+
+    // for all unused blocks:
+    for unused_binding in unused_blocks {
+        let removal_index = unused_binding.0;
+        // #1. Rename references to the blocks after the current block to match their new index
+        // (drops by 1)
+        for further_index in removal_index + 1..ir.code.len() {
+            let target_index = further_index - 1;
+            let old = BlockBinding(further_index);
+            let new = BlockBinding(target_index);
+
+            refactor::rename_block(ir, old, new);
+        }
+
+        // #2. Now we can safely remove the block
+        ir.code.remove(removal_index);
     }
 }
+
+///// Block aliasing occurs when a block only has a single branch as its code
+// fn remove_block_aliasing(ir: &mut IR) {
+//     // collect everything into a vec to end the ref
+//     let aliases: Vec<_> = ir
+//         .code
+//         .iter()
+//         .enumerate()
+//         .filter_map(|(block_index, block)| {
+//             // single branch
+//             if block.statements.is_empty() {
+//                 // jumps directly to a block
+//                 if let BlockEnd::Branch(Branch::Unconditional { target }) = block.end {
+//                     Some((BlockBinding(block_index), target))
+//                 } else {
+//                     None
+//                 }
+//             } else {
+//                 None
+//             }
+//         })
+//         .collect();
+//
+//     for (aliased_block, alias_target) in dbg!(aliases) {
+//         // #1. Patch the code
+//         rename_block(ir, aliased_block, alias_target);
+//         // #2. Patch the backwards map:
+//         // move the sources from `aliased_block` to `alias_target`
+//         if let Some(sources) = ir.backwards_map.remove(&aliased_block) {
+//             let target_sources = ir
+//                 .backwards_map
+//                 .get_mut(&alias_target)
+//                 .expect("Malformed backwards map: missing back reference to already detected jump");
+//             for source in sources {
+//                 if !target_sources.contains(&source) {
+//                     target_sources.push(source);
+//                 }
+//             }
+//         }
+//     }
+// }
