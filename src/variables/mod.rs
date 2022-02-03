95b267bc612924ca5948841f8f6cb953debd8844
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-02-03 22:52:09 +0100

    fix(variables, blocks): Blocks didn't respect variables allocated by other blocks.

--- src/variables/mod.rs
+++ src/variables/mod.rs
@@ -36,7 +36,6 @@ impl std::error::Error for VarError {}
 
 // fn walk_block<'code>(block: Vec<(Statement<'code>, Span)>, )
 
-type UsedMap<'code> = HashMap<&'code str, bool>;
 type IndexMap<'code> = HashMap<&'code str, usize>;
 
 pub fn convert_program<'code>(
@@ -45,6 +44,55 @@ pub fn convert_program<'code>(
 ) -> Result<codegen::hlir::Program<'code>, error::Error<VarError>> {
     let Program(function) = program;
     convert_function(function, source_meta).map(codegen::hlir::Program)
+}
+
+struct Env<'code> {
+    pub offset: usize,
+    pub max_offset: usize,
+    pub indices: Vec<IndexMap<'code>>,
+}
+
+impl<'code> Env<'code> {
+    pub fn new() -> Self {
+        Self {
+            offset: 0,
+            max_offset: 0,
+            indices: Vec::new(),
+        }
+    }
+
+    fn current_map(&mut self) -> &mut IndexMap<'code> {
+        if self.indices.is_empty() {
+            self.push_map();
+        }
+        self.indices.last_mut().unwrap()
+    }
+
+    fn add(&mut self, name: &'code str) -> VarRes<usize> {
+        let offset = self.offset;
+        if self.current_map().insert(name, offset).is_some() {
+            return Err(error::Error::new(VarError::Redeclared(name.to_string())));
+        }
+        self.offset += 1;
+        self.max_offset = self.offset.max(self.max_offset);
+        Ok(offset)
+    }
+
+    fn get(&self, name: &str) -> Option<usize> {
+        self.indices
+            .iter()
+            .rev()
+            .find_map(|map| map.get(name))
+            .copied()
+    }
+
+    fn push_map(&mut self) {
+        self.indices.push(IndexMap::new())
+    }
+
+    fn pop_map(&mut self) {
+        self.offset -= self.indices.pop().expect("popping from empty env").len();
+    }
 }
 
 fn convert_function<'code>(
@@ -56,284 +104,152 @@ fn convert_function<'code>(
         body: Block { statements: block },
     } = func;
 
-    let dummy_index_map = IndexMap::new();
-    let (body, var_amt) = convert_block(block, 0, &dummy_index_map, source_meta)?;
+    let mut env = Env::new();
+    let body = assign_block_indices(block, &mut env, source_meta)?;
 
     Ok(codegen::hlir::Function {
         name,
         body,
-        var_amt,
+        var_amt: env.max_offset,
     })
 }
 
-fn convert_block<'code>(
+type VarRes<T> = Result<T, error::Error<VarError>>;
+
+fn assign_block_indices<'code>(
     block: Vec<(Statement<'code>, Span)>,
-    variables_offset: usize,
-    index_map: &IndexMap<'code>,
+    env: &mut Env<'code>,
     source_meta: &SourceMetadata,
-) -> Result<(Vec<codegen::hlir::Statement>, usize), error::Error<VarError>> {
-    let mut used_map = UsedMap::new();
-    let mut total_statements = 0usize;
-
-    // pass #1: Make a pass to know which variables are being used. This ignores unknown variables
-    for (statement, statement_span) in &block {
-        total_statements += match statement {
-            // declarations when converting the block are treated differently as
-            // we need to detect redeclarations
-            Statement::DeclareVar {
-                name: Source { source: name, .. },
-                init,
-            } => {
-                // redeclarations here are penalized
-                if used_map.contains_key(name) {
-                    return Err(error::Error::new(VarError::Redeclared(name.to_string()))
-                        .with_source(*statement_span, source_meta));
-                }
-                if let Some((init, _)) = init {
-                    walk_expr_used_vars(init, &mut used_map);
-                }
-                used_map.insert(name, false);
-                0usize
-            }
-            statement => walk_statement_used_vars(statement, &mut used_map),
-        };
-    }
-
-    // now create indices for all those variables that were used
-    let index_map: IndexMap = index_map
-        .clone()
+) -> VarRes<Vec<codegen::hlir::Statement>> {
+    env.push_map();
+    let result = block
         .into_iter()
-        .chain(
-            used_map
-                .into_iter()
-                .filter(|(_, is_used)| *is_used)
-                .enumerate()
-                .map(|(index, (name, _))| (name, index + variables_offset)),
-        )
-        .collect();
-
-    // pass #2: Convert all the statements to HLIR. This pass does error for unknown/redeclared variables
-
-    // full block length is the max length it could have. It can have *less*, but not more than those
-    let mut final_hlir_code = Vec::with_capacity(total_statements);
-    let mut max_extra_amount = 0usize;
-
-    for (statement, _) in block {
-        // NOTE: these potential errors don't need a backup source because all of the paths of
-        // `stmt_assign_indices` assign a source
-        let (hlir, extra_variables) = stmt_assign_indices(
-            statement,
-            index_map.len() + variables_offset,
-            &index_map,
-            source_meta,
-        )?;
-        final_hlir_code.extend(hlir);
-        max_extra_amount = max_extra_amount.max(extra_variables);
-    }
-
-    Ok((final_hlir_code, index_map.len() + max_extra_amount))
+        .map(|(stmt, _)| assign_statement_indices(stmt, env, source_meta))
+        .fold(Ok(Vec::new()), |v, other| {
+            let mut v = v?;
+            let other = other?;
+            v.reserve(other.len());
+            v.extend(other);
+            Ok(v)
+        });
+    env.pop_map();
+    result
 }
 
-// ------------
-// walks for used variables. Note that any of these don't add any variables
+fn assign_statement_indices<'code>(
+    stmt: Statement<'code>,
+    env: &mut Env<'code>,
+    source_meta: &SourceMetadata,
+) -> VarRes<Vec<codegen::hlir::Statement>> {
+    match stmt {
+        Statement::Return((expr, expr_span)) => {
+            let ret_expr = assign_expr_indices(expr, env, source_meta)
+                .map_err(|err| err.with_backup_source(expr_span, source_meta))?;
+            Ok(vec![codegen::hlir::Statement::Return(ret_expr)])
+        }
+        Statement::SingleExpr((expr, expr_span)) => {
+            let single_expr = assign_expr_indices(expr, env, source_meta)
+                .map_err(|err| err.with_backup_source(expr_span, source_meta))?;
 
-// gets the max number of statements per statement kind as well
-fn walk_statement_used_vars<'code>(
-    statement: &Statement<'code>,
-    used_map: &mut UsedMap<'code>,
-) -> usize {
-    match statement {
-        Statement::Return((expr, _)) => {
-            walk_expr_used_vars(expr, used_map);
-            1
+            Ok(vec![codegen::hlir::Statement::Single(single_expr)])
         }
-        Statement::SingleExpr((expr, _)) => {
-            walk_expr_used_vars(expr, used_map);
-            1
-        }
-        Statement::DeclareVar { name: _, init } => {
-            if let Some((init, _)) = init {
-                walk_expr_used_vars(init, used_map);
-            }
-            1
+        Statement::DeclareVar {
+            name: Source { span, source: name },
+            init,
+        } => {
+            // first, compile the init expr if it is there
+            let init = init
+                .map(|(expr, expr_span)| {
+                    assign_expr_indices(expr, env, source_meta)
+                        .map_err(|err| err.with_backup_source(expr_span, source_meta))
+                })
+                .transpose()?;
+
+            // now we can safely add our variable to the block
+            let index = env
+                .add(name)
+                .map_err(|err| err.with_backup_source(span, source_meta))?;
+
+            // wrap the init in an assign statement and we're good to go!
+            Ok(init
+                .map(|init| {
+                    codegen::hlir::Statement::Single(codegen::hlir::Expr::Binary {
+                        operator: BinaryOp::Assignment { op: None },
+                        lhs: Box::new(codegen::hlir::Expr::Variable { index }),
+                        rhs: Box::new(init),
+                        branch_depends_on_result: false,
+                    })
+                })
+                .into_iter()
+                .collect())
         }
         Statement::Block(block) => {
-            walk_block_used_vars(block, used_map);
-            block.len()
+            let block = assign_block_indices(block, env, source_meta)?;
+            Ok(block)
         }
         Statement::IfStatement {
-            condition: (condition, _),
+            condition: (condition, condition_span),
             true_branch: (true_branch, _),
             false_branch,
         } => {
-            walk_expr_used_vars(condition, used_map);
-            let true_branch_stmts = walk_statement_used_vars(true_branch, used_map);
-            if let Some((false_branch, _)) = false_branch {
-                true_branch_stmts.max(walk_statement_used_vars(false_branch, used_map))
-            } else {
-                true_branch_stmts
-            }
-        }
-    }
-}
-
-fn walk_block_used_vars<'code>(block: &[(Statement<'code>, Span)], used_map: &mut UsedMap<'code>) {
-    for (statement, _) in block {
-        walk_statement_used_vars(statement, used_map);
-    }
-}
-
-fn walk_expr_used_vars<'code>(expr: &Expr<'code>, used_map: &mut UsedMap<'code>) {
-    match expr {
-        Expr::Variable {
-            name: Source { source: name, .. },
-        } => {
-            if let Some(is_used) = used_map.get_mut(name) {
-                *is_used = true;
-            }
-        }
-        Expr::Constant(_) => (),
-        Expr::Unary {
-            operator: _,
-            expr: (expr, _),
-        } => walk_expr_used_vars(expr, used_map),
-        Expr::Binary {
-            operator: _,
-            lhs: (lhs, _),
-            rhs: (rhs, _),
-        } => {
-            walk_expr_used_vars(lhs, used_map);
-            walk_expr_used_vars(rhs, used_map);
-        }
-    }
-}
-
-// ----- assignment routines ----
-
-fn stmt_assign_indices<'code>(
-    statement: Statement<'code>,
-    variables_offset: usize,
-    index_map: &IndexMap<'code>,
-    source_meta: &SourceMetadata,
-) -> Result<(Vec<codegen::hlir::Statement>, usize), error::Error<VarError>> {
-    match statement {
-        Statement::Return((expr, expr_span)) => {
-            let expr = expr_assign_indices(expr, index_map, source_meta)
-                .map_err(|err| err.with_backup_source(expr_span, source_meta))?;
-            // no variables were added
-            Ok((vec![codegen::hlir::Statement::Return(expr)], 0))
-        }
-        Statement::SingleExpr((expr, expr_span)) => {
-            let expr = expr_assign_indices(expr, index_map, source_meta)
-                .map_err(|err| err.with_backup_source(expr_span, source_meta))?;
-            // no variables were added
-            Ok((vec![codegen::hlir::Statement::Single(expr)], 0))
-        }
-        Statement::DeclareVar {
-            name: Source { source: name, .. },
-            init,
-        } => {
-            // only do something if an init expression is given, otherwise
-            // it was just an annotation for the variable processor
-            let result_vec = if let Some((init_expr, init_span)) = init {
-                let init = expr_assign_indices(init_expr, index_map, source_meta)
-                    .map_err(|err| err.with_backup_source(init_span, source_meta))?;
-                vec![codegen::hlir::Statement::Single(
-                    // if the variable was not discarded, then give an assignment expression,
-                    // otherwise just give it the init expression
-                    if let Some(&index) = index_map.get(name) {
-                        codegen::hlir::Expr::Binary {
-                            operator: BinaryOp::Assignment { op: None },
-                            lhs: Box::new(codegen::hlir::Expr::Variable { index }),
-                            rhs: Box::new(init),
-                            branch_depends_on_result: false,
-                        }
-                    } else {
-                        init
-                    },
-                )]
-            } else {
-                Vec::new()
-            };
-            Ok((result_vec, 0))
-        }
-        Statement::Block(block) => convert_block(block, variables_offset, index_map, source_meta),
-        Statement::IfStatement {
-            condition: (condition, condition_span),
-            true_branch: (true_branch, true_branch_span),
-            false_branch,
-        } => {
-            let condition = expr_assign_indices(condition, index_map, source_meta)
+            let condition = assign_expr_indices(condition, env, source_meta)
                 .map_err(|err| err.with_backup_source(condition_span, source_meta))?;
-            let (true_branch, true_extra_variables) =
-                stmt_assign_indices(*true_branch, variables_offset, index_map, source_meta)
-                    .map_err(|err| err.with_backup_source(true_branch_span, source_meta))?;
-            let (false_branch, false_extra_variables) = if let Some((branch, extra_vars)) =
-                false_branch
-                    .map(|(stmt, span)| {
-                        stmt_assign_indices(*stmt, variables_offset, index_map, source_meta)
-                            .map_err(|err| err.with_backup_source(span, source_meta))
-                    })
-                    .transpose()?
-            {
-                (Some(branch), extra_vars)
-            } else {
-                (None, 0)
-            };
-
-            Ok((
-                vec![codegen::hlir::Statement::IfStatement {
-                    condition,
-                    true_branch,
-                    false_branch,
-                }],
-                true_extra_variables.max(false_extra_variables),
-            ))
+            let true_branch = assign_statement_indices(*true_branch, env, source_meta)?;
+            let false_branch = false_branch
+                .map(|(false_branch, _)| assign_statement_indices(*false_branch, env, source_meta))
+                .transpose()?;
+            Ok(vec![codegen::hlir::Statement::IfStatement {
+                condition,
+                true_branch,
+                false_branch,
+            }])
         }
     }
 }
 
-fn expr_assign_indices<'code>(
+fn assign_expr_indices<'code>(
     expr: Expr<'code>,
-    index_map: &IndexMap<'code>,
+    scope: &Env<'code>,
     source_meta: &SourceMetadata,
-) -> Result<codegen::hlir::Expr, error::Error<VarError>> {
+) -> VarRes<codegen::hlir::Expr> {
     match expr {
+        Expr::Constant(ctant) => Ok(codegen::hlir::Expr::Constant(ctant)),
         Expr::Variable {
-            name: Source { source: name, .. },
+            name: Source {
+                span: _,
+                source: name,
+            },
         } => {
-            let index = index_map
+            let index = scope
                 .get(name)
-                .copied()
                 .ok_or_else(|| error::Error::new(VarError::Undeclared(name.to_string())))?;
             Ok(codegen::hlir::Expr::Variable { index })
-        }
-        Expr::Constant(c) => Ok(codegen::hlir::Expr::Constant(c)),
-        Expr::Unary {
-            operator,
-            expr: (expr, inner_span),
-        } => {
-            let inner = expr_assign_indices(*expr, index_map, source_meta)
-                .map_err(|err| err.with_backup_source(inner_span, source_meta))?;
-            Ok(codegen::hlir::Expr::Unary {
-                operator,
-                inner: Box::new(inner),
-            })
         }
         Expr::Binary {
             operator,
             lhs: (lhs, lhs_span),
             rhs: (rhs, rhs_span),
         } => {
-            let lhs = expr_assign_indices(*lhs, index_map, source_meta)
+            let lhs = assign_expr_indices(*lhs, scope, source_meta)
                 .map_err(|err| err.with_backup_source(lhs_span, source_meta))?;
-            let rhs = expr_assign_indices(*rhs, index_map, source_meta)
+            let rhs = assign_expr_indices(*rhs, scope, source_meta)
                 .map_err(|err| err.with_backup_source(rhs_span, source_meta))?;
             Ok(codegen::hlir::Expr::Binary {
                 operator,
                 lhs: Box::new(lhs),
                 rhs: Box::new(rhs),
                 branch_depends_on_result: false,
+            })
+        }
+        Expr::Unary {
+            operator,
+            expr: (expr, expr_span),
+        } => {
+            let expr = assign_expr_indices(*expr, scope, source_meta)
+                .map_err(|err| err.with_backup_source(expr_span, source_meta))?;
+            Ok(codegen::hlir::Expr::Unary {
+                operator,
+                inner: Box::new(expr),
             })
         }
     }
