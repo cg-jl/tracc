91eb97a6037f63aa16f4d32ee122636f12bc2f11
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-02-04 20:08:27 +0100

    registers: Create usage map from bindings

--- src/codegen/registers.rs
+++ src/codegen/registers.rs
@@ -1,6 +1,12 @@
 //! Register analysis of the code
 
-use crate::intermediate::{Binding, IR};
+use std::collections::{HashMap, HashSet};
+
+use crate::intermediate::{
+    analysis, Binding, BlockBinding, BlockEnd, Branch, Statement, Value, IR,
+};
+
+use super::assembly::Condition;
 
 // #1. Get what each binding is used for, call it hints:
 //      - returning from a function:
@@ -13,8 +19,11 @@ use crate::intermediate::{Binding, IR};
 //      - is an argument to a call
 //          - we might want to compute the value in its corresponding register
 //
-// #2. Take the hints and assign the register indices.
+// #2. Take the hints and assign the registers.
 //     allocate memory for mid-saving them, if necessary (accounting for their size)
+//     note that bindings that were allocated by the memory module will be given to the register
+//     allocator so that it knows it mustn't allocate a general purpose register for those (they are already
+//     assigned to `sp`)
 //
 // #3. Check for callee-saved registers (might need extra memory for those)
 //     and reserve full-register size per each
@@ -32,11 +41,79 @@ use crate::intermediate::{Binding, IR};
 enum Hint {
     /// The binding is used in a `ret` statement
     Return,
-    /// The binding is used in a phi function: we'd want it to have the same register as the other bindings
-    Phi(Vec<Binding>),
+    /// The binding is used to branch to different blocks
+    CmpResult(Condition),
+    /// The binding is used in a phi function to other binding
+    Phi { target: Binding },
+    /// The binding is a pointer to a stack-allocated frame, ignore it
+    AllocPtr,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum Usage {
+    /// the binding is used in a computation of other binding
+    Binding(Binding),
+    /// the binding is used in a store
+    Store(Binding),
+    /// the binding is used in a return statement
+    Return,
+    /// the binding is used as a switch for branch
+    Branch,
+}
+type UsageMap = HashMap<Binding, Vec<Usage>>;
+
+// get what each binding depends on
+fn get_usage_map(ir: &IR) -> UsageMap {
+    let mut usage_map = UsageMap::new();
+
+    for block in &ir.code {
+        for statement in &block.statements {
+            match statement {
+                Statement::Store {
+                    mem_binding,
+                    binding,
+                    ..
+                } => usage_map
+                    .entry(*binding)
+                    .or_default()
+                    .push(Usage::Store(*mem_binding)),
+                Statement::Assign { index, value } => {
+                    use analysis::BindingUsage;
+                    for dep in value.binding_deps() {
+                        usage_map
+                            .entry(dep)
+                            .or_default()
+                            .push(Usage::Binding(*index));
+                    }
+                }
+            }
+        }
+
+        match block.end {
+            BlockEnd::Return(ret) => usage_map.entry(ret).or_default().push(Usage::Return),
+            BlockEnd::Branch(Branch::Conditional { flag, .. }) => {
+                usage_map.entry(flag).or_default().push(Usage::Branch)
+            }
+            BlockEnd::Branch(Branch::Unconditional { .. }) => {}
+        }
+    }
+    usage_map
+}
+
+//fn uses_from_deps(deps: &HashMap<Binding, Vec<Binding>>) -> UsageMap {}
+
+// TODO: figure out how cpu status flags are affected by each binding and if the last modifier to
+// the flags was the same binding that is indicating a `CmpResult`, we can avoid allocating a
+// register for it. If any of the binding's uses requires a register (or discards the flags) then
+// the flag binding will be used on a register and re-checked in case of a branch where the state
+// was discarded.
+//
+// We have 3 types of bindings:
+//  - registers
+//  - cpu state flags
+//  - stack pointer bindings (with a known offset)
 
 pub fn debug_what_im_doing(ir: &IR) {
-    dbg!(ir);
+    let usage_map = get_usage_map(ir);
+    dbg!(ir, usage_map);
 }
