4080e2de17dea4215f044ad0b4cc2590d2efd8f7
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-04-01 22:27:24 +0200

    feat(codegen_context, expr/bit): Bit expressions use context

--- src/codegen/expr/bit.rs
+++ src/codegen/expr/bit.rs
@@ -1,74 +1,59 @@
 use crate::{
     ast::BitOp,
     codegen::{
-        assembly::{BitSize, Data, Instruction, Memory},
+        assembly::{BitSize, Instruction},
         hlir::Expr,
-        registers::{RegisterDescriptor, RegisterManager, UsageContext},
-        stack::StackManager,
-        AssemblyOutput,
+        AssemblyOutput, CompileWith, CompilerContext,
     },
 };
 
-use super::compile_expr;
+use super::compile_expr_as_data;
 
 pub fn compile_bit_op(
     bitop: BitOp,
-    target: RegisterDescriptor,
+    ctx: &mut CompilerContext,
     lhs: Expr,
     rhs: Expr,
-    registers: &mut RegisterManager,
-    stack: &mut StackManager,
-    var_ctx: &[Memory],
-    is_ignored: bool,
 ) -> AssemblyOutput {
     // kind of same stuff as arithmetic operations
-    compile_expr(lhs, target, registers, stack, var_ctx, is_ignored).chain(if !is_ignored {
-        let (compute_rhs, rhs_data) = if let Expr::Constant(b) = rhs {
-            (AssemblyOutput::new(), Data::Immediate(b as u64))
-        } else {
-            registers.locking_register(target, |registers| {
-                let rhs_target = registers.get_suitable_register(UsageContext::Normal);
-                let compute_rhs = compile_expr(rhs, rhs_target, registers, stack, var_ctx, false);
-                (
-                    compute_rhs,
-                    Data::Register(rhs_target.as_immutable(BitSize::Bit32)),
-                )
-            })
-        };
-        compute_rhs.chain(registers.using_register_mutably(
-            stack,
-            target,
-            BitSize::Bit32,
-            |_stack, _registers, target| match bitop {
-                BitOp::And => Instruction::And {
-                    target,
-                    lhs: target.into(),
-                    rhs: rhs_data,
-                },
-                BitOp::Or => Instruction::Orr {
-                    target,
-                    lhs: target.into(),
-                    rhs: rhs_data,
-                },
-                BitOp::Xor => Instruction::Eor {
-                    target,
-                    lhs: target.into(),
-                    rhs: rhs_data,
-                    bitmask: BitSize::Bit32.full_bits(),
-                },
-                BitOp::LeftShift => Instruction::Lsl {
-                    target,
-                    lhs: target.into(),
-                    rhs: rhs_data,
-                },
-                BitOp::RightShift => Instruction::Lsr {
-                    target,
-                    lhs: target.into(),
-                    rhs: rhs_data,
-                },
-            },
-        ))
-    } else {
-        compile_expr(rhs, target, registers, stack, var_ctx, true)
-    })
+    lhs.compile(ctx)
+        .chain(ctx.checking_ignored(|ctx, is_ignored| {
+            if !is_ignored {
+                let (compute_rhs, rhs_data) = compile_expr_as_data(ctx, rhs);
+                compute_rhs.chain(ctx.using_register_mutably(
+                    ctx.target,
+                    BitSize::Bit32,
+                    |_ctx, target| match bitop {
+                        BitOp::And => Instruction::And {
+                            target,
+                            lhs: target.into(),
+                            rhs: rhs_data,
+                        },
+                        BitOp::Or => Instruction::Orr {
+                            target,
+                            lhs: target.into(),
+                            rhs: rhs_data,
+                        },
+                        BitOp::Xor => Instruction::Eor {
+                            target,
+                            lhs: target.into(),
+                            rhs: rhs_data,
+                            bitmask: BitSize::Bit32.full_bits(),
+                        },
+                        BitOp::LeftShift => Instruction::Lsl {
+                            target,
+                            lhs: target.into(),
+                            rhs: rhs_data,
+                        },
+                        BitOp::RightShift => Instruction::Lsr {
+                            target,
+                            lhs: target.into(),
+                            rhs: rhs_data,
+                        },
+                    },
+                ))
+            } else {
+                ctx.with_ignoring(true, |ctx| rhs.compile(ctx))
+            }
+        }))
 }
