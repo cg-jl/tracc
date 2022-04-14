48f4184077c3f89047ec3a2584d75143a37c212c
Cyber Gsus <cyber.gsuscode@gmail.com>
2022-04-14 11:44:17 +0200

    codegen: Add `&mut` functions for AssemblyOutput

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -14,16 +14,44 @@ impl AssemblyOutput {
     }
 
     pub fn cons(mut self, value: impl Into<assembly::Assembly>) -> Self {
+        self.push_front(value);
+        self
+    }
+
+    pub fn push_front(&mut self, value: impl Into<assembly::Assembly>) -> &mut Self {
         self.0.push_front(value.into());
         self
     }
 
-    pub fn push(mut self, value: impl Into<assembly::Assembly>) -> Self {
+    pub fn push_back(&mut self, value: impl Into<assembly::Assembly>) -> &mut Self {
         self.0.push_back(value.into());
         self
     }
 
-    pub fn extend<T>(mut self, values: impl IntoIterator<Item = T>) -> Self
+    pub fn chain_back<T>(mut self, values: impl IntoIterator<Item = T>) -> Self
+    where
+        T: Into<assembly::Assembly>,
+    {
+        for v in values {
+            self.0.push_front(v.into());
+        }
+        self
+    }
+
+    pub fn chain_one(mut self, value: impl Into<assembly::Assembly>) -> Self {
+        self.push_back(value);
+        self
+    }
+
+    pub fn chain<T>(mut self, values: impl IntoIterator<Item = T>) -> Self
+    where
+        T: Into<assembly::Assembly>,
+    {
+        self.extend(values);
+        self
+    }
+
+    pub fn extend<T>(&mut self, values: impl IntoIterator<Item = T>) -> &mut Self
     where
         T: Into<assembly::Assembly>,
     {
@@ -47,6 +75,19 @@ where
 {
     fn from(item: T) -> Self {
         Self::new().cons(item)
+    }
+}
+
+impl FromIterator<AssemblyOutput> for AssemblyOutput {
+    fn from_iter<T: IntoIterator<Item = AssemblyOutput>>(iter: T) -> Self {
+        iter.into_iter()
+            .fold(AssemblyOutput::new(), AssemblyOutput::chain)
+    }
+}
+
+impl FromIterator<assembly::Assembly> for AssemblyOutput {
+    fn from_iter<T: IntoIterator<Item = assembly::Assembly>>(iter: T) -> Self {
+        Self(VecDeque::from_iter(iter))
     }
 }
 
@@ -90,38 +131,46 @@ pub fn codegen_function(function_name: String, ir: IR) -> AssemblyOutput {
         "TODO: implement moves to return register (or generic move to register)"
     );
 
-    let mut output = AssemblyOutput::new()
+    let r#final: AssemblyOutput = ir
+        .code
+        .into_iter()
+        .enumerate()
+        .flat_map(|(block_index, block)| {
+            let has_return = matches!(block.end, BlockEnd::Return(_));
+            let mut compiled_block = compile_block(block, &memory, &registers);
+            if has_return && mem_size != 0 {
+                compiled_block.0.insert(
+                    compiled_block.0.len() - 1,
+                    assembly::Instruction::Add {
+                        target: assembly::Register::StackPointer,
+                        lhs: assembly::Register::StackPointer,
+                        rhs: assembly::Data::Immediate(mem_size as u64),
+                    }
+                    .into(),
+                );
+            }
+
+            compiled_block.cons(BasicBlockLabel(block_index))
+        })
+        .collect();
+
+    r#final
+        .chain_back(if mem_size == 0 {
+            None
+        } else {
+            Some(assembly::Instruction::Sub {
+                target: assembly::Register::StackPointer,
+                lhs: assembly::Register::StackPointer,
+                rhs: assembly::Data::Immediate(mem_size as u64),
+            })
+        })
         // declare function as global for linkage
-        .push(assembly::Directive::Global(function_name.clone()))
-        .push(assembly::Directive::Type(
+        .cons(assembly::Assembly::Label(function_name.clone()))
+        .cons(assembly::Directive::Type(
             function_name.clone(),
             "function".into(),
         ))
-        .push(assembly::Assembly::Label(function_name));
-
-    for (block_index, block) in ir.code.into_iter().enumerate() {
-        output = output
-            .push(BasicBlockLabel(block_index))
-            .extend(compile_block(block, &memory, &registers))
-    }
-
-    if mem_size != 0 {
-        // add the proper instructions for setting up the stack.
-        // TODO: callee-saved register presaving
-        output
-            .cons(assembly::Instruction::Sub {
-                target: assembly::Register::StackPointer,
-                lhs: assembly::Register::StackPointer,
-                rhs: assembly::Data::immediate(mem_size as u64, assembly::BitSize::Bit64),
-            })
-            .push(assembly::Instruction::Add {
-                target: assembly::Register::StackPointer,
-                lhs: assembly::Register::StackPointer,
-                rhs: assembly::Data::immediate(mem_size as u64, assembly::BitSize::Bit64),
-            })
-    } else {
-        output
-    }
+        .cons(assembly::Directive::Global(function_name))
 }
 
 fn compile_block(
@@ -131,7 +180,7 @@ fn compile_block(
 ) -> AssemblyOutput {
     let mut output = AssemblyOutput::new();
     for statement in block.statements {
-        output = output.extend(match statement {
+        output = output.chain(match statement {
             Statement::Assign { index, value } => {
                 let register = registers[&index];
                 compile_value(value, register, memory, registers)
@@ -157,7 +206,7 @@ fn compile_block(
     match block.end {
         BlockEnd::Branch(branch) => match branch {
             Branch::Unconditional { target } => {
-                output = output.push(assembly::Branch::Unconditional {
+                output = output.chain_one(assembly::Branch::Unconditional {
                     register: None,
                     label: assembly::BasicBlockLabel::new(target.0),
                 })
@@ -170,25 +219,25 @@ fn compile_block(
                 // TODO: map with known already touched CPU flags at the end of every block, and
                 // the result they computed, if any.
                 output = output
-                    .push(assembly::Instruction::Cmp {
+                    .chain_one(assembly::Instruction::Cmp {
                         register: assembly::Register::from_id(
                             registers[&flag],
                             assembly::BitSize::Bit32,
                         ),
                         data: assembly::Data::immediate(0, assembly::BitSize::Bit32),
                     })
-                    .push(assembly::Branch::Conditional {
+                    .chain_one(assembly::Branch::Conditional {
                         condition: assembly::Condition::Equals,
                         label: assembly::BasicBlockLabel::new(target_false.0),
                     })
-                    .push(assembly::Branch::Unconditional {
+                    .chain_one(assembly::Branch::Unconditional {
                         register: None,
                         label: assembly::BasicBlockLabel::new(target_true.0),
                     });
             }
         },
         // TODO: make sure that the returned binding is in the place it should.
-        BlockEnd::Return(ret) => output = output.push(assembly::Instruction::Ret),
+        BlockEnd::Return(ret) => output = output.chain_one(assembly::Instruction::Ret),
     }
     output
 }
@@ -228,7 +277,7 @@ fn compile_value(
             register: assembly::Register::from_id(registers[&lhs], assembly::BitSize::Bit32),
             data: could_be_constant_to_data(rhs, registers),
         })
-        .push(assembly::Instruction::Cset {
+        .chain_one(assembly::Instruction::Cset {
             target: assembly::Register::from_id(target_register, assembly::BitSize::Bit32),
             condition,
         }),
